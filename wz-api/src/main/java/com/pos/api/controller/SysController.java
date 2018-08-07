a139c988c26c04f17de045dc11e5febb2e002761
taodongdong <taodongdong@example.com>
2018-08-07 16:19:11 +0800

    api刷新缓存接口修改

---
+++ wz-api/src/main/java/com/pos/api/controller/SysController.java
@@ -0,0 +1,48 @@
+package com.pos.api.controller;
+
+import com.wz.cashloan.core.common.context.Constant;
+import com.wz.cashloan.core.common.util.CacheUtil;
+import com.wz.cashloan.core.common.util.ServletUtils;
+import com.wz.cashloan.core.common.web.controller.BaseController;
+import com.wz.cashloan.system.permission.annotation.RequiresPermission;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * API System Controller
+ * 
+ * @author gc
+ * @version 1.0.0
+ * @date 2017年4月29日 下午3:47:24
+ * Copyright 杭州融都科技股份有限公司 All Rights Reserved
+ * 官方网站：www.erongdu.com
+ * 未经授权不得进行修改、复制、出售及商业使用
+ */
+
+@Controller
+@Scope("prototype")
+public class SysController extends BaseController {
+    @Resource
+    private CacheUtil cacheUtil;
+    /**
+     * 重加载系统配置数据
+     * 
+     * @throws Exception
+     */
+    @RequestMapping("/system/config/reload.htm")
+    @RequiresPermission(code = "system:config:reload", name = "系统管理-系统参数设置-缓存数据重加载")
+    public void reload() throws Exception {
+        // 调用缓存辅助类 重加载系统配置数据
+    		cacheUtil.initSysConfig();
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		returnMap.put(Constant.RESPONSE_CODE, Constant.SUCCEED_CODE_VALUE);
+		returnMap.put(Constant.RESPONSE_CODE_MSG, Constant.OPERATION_SUCCESS);
+		ServletUtils.writeToResponse(response, returnMap);
+    }
+    
+}
